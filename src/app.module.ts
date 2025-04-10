bcfb9fcce756eea74e07a47a827d74d3dfa25300
vivi2901 <202000200@est.umss.edu>
2025-04-10 09:14:41 -0400

    categoriesResource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, ProductsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
